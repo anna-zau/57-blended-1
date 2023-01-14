3894c933e38e1c294c97d5000f1f87c458489737
Andrii Popov <popov.andrey80@gmail.com>
2023-01-14 11:29:42 +0200

    add searchCountries

--- src/pages/CountrySearch.jsx
+++ src/pages/CountrySearch.jsx
@@ -1,17 +1,29 @@
-import {
-  Container,
-  SearchForm,
-  Section,
-  Heading,
-  Loader,
-  CountryList,
-} from 'components';
+import { Container, SearchForm, Section, Heading, Loader, CountryList } from 'components';
+import { fetchByRegion } from 'service/country-service';
+import { useState, useEffect } from 'react';
 
 export const CountrySearch = () => {
+  const [countryName, setCountryName] = useState('');
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    if (!countryName) {
+      return;
+    }
+
+    fetchByRegion(countryName).then(resp => setCountries(resp));
+  }, [countryName]);
+
+  const handleSubmit = region => {
+    console.log(region);
+    setCountryName(region);
+  };
+
   return (
     <Section>
       <Container>
-        <h2>CountrySearch</h2>
+        <SearchForm onSubmit={handleSubmit} />
+        <CountryList countries={countries} />
       </Container>
     </Section>
   );
