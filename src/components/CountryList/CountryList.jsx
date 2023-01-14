10124e0a25f43e57f837199453fdf586a81e5c58
anna-zau <annazauholnikova555@gmail.com>
2023-01-14 11:03:44 +0200

    added countries + home + routes

--- src/components/CountryList/CountryList.jsx
+++ src/components/CountryList/CountryList.jsx
@@ -1,5 +1,18 @@
+import { Link } from 'react-router-dom';
 import { Grid, GridItem } from 'components';
 
 export const CountryList = ({ countries }) => {
-  return <h2>CountryList</h2>;
+  return (
+    <Grid>
+      {countries.map(({ id, country, flag }) => {
+        return (
+          <GridItem key={id}>
+            <Link to={`/country/${id}`}>
+              <img src={flag} alt={country} />
+            </Link>
+          </GridItem>
+        );
+      })}
+    </Grid>
+  );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
+import { useState, useEffect } from 'react';
+
 import { Container, CountryList, Heading, Loader, Section } from 'components';
 
+import { getCountries } from '../service/country-service';
+
 export const Home = () => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    getCountries().then(data => setCountries(data));
+  }, []);
+
   return (
     <Section>
       <Container>
-        <h2>Home</h2>
+        <CountryList countries={countries} />
       </Container>
     </Section>
   );
